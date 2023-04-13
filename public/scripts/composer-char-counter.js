8e248759efb8a27a6e4c9cf676bfaa32d4bad5e1
Brittany Cowper <brittferren@gmail.com>
2023-04-13 11:34:26 -0500

    restyle new-tweet section as a CSS grid

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,22 @@
+/* eslint-env jquery */
+/* eslint-env browser */
+
 $(document).ready(function() {
   const textarea = $('#tweet-text');
   textarea.on("input", function() {
     const charLimit = 140;
-    let charCount = $('div').find('.counter');
+    //$(this) refers to textarea
     let charInput = $(this).val().length;
+    //$(this).siblings = textarea siblings in DOM tree, identifying class 'counter' here
+    let charCount = $(this).siblings('.counter');
+    //if input exceeds limit, create new class to assign red counter into the negatives
     if (charInput > charLimit) {
       charCount.addClass('over-limit');
+    //if input is within limit, remove class and counter is original colour
     } else if (charInput <= charLimit) {
       charCount.removeClass('over-limit');
     }
-    charCount.text(charLimit - charInput); 
-  })
+    //set the value out output counter
+    charCount.text(charLimit - charInput);
+  });
 });
