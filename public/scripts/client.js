43be45a043170f72461293d8b0b3f8acb25f3245
Brittany Cowper <brittferren@gmail.com>
2023-04-19 12:19:58 -0500

    dynamically set tweets via createTweetElement and RenderTweets fns

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,39 +7,53 @@
 /* eslint-env jquery */
 /* eslint-env browser */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
-$(document).ready(function() {
-  const createTweetElement = function(tweet) {
-    const $tweet = $("<article>").addClass("tweet-container");
-    const content =
-    `<header>
-      <img src=${tweet.user.avatars} alt="my avatar">
-      <h3>${tweet.name}</h3>
-      <address>${tweet.user.handle}</address>
-    </header>
-    <p>${tweet.content.text}</p>
-    <footer>
-      <time>${tweetTimestamp(tweet)}</time>
-      <span class="fa-solid fa-flag" style="color:#4056A1">
-        <i class="fa-solid fa-retweet" style="color:#4056A1"></i>
-        <i class="fa-solid fa-heart" style="color:#4056A1"></i>
-      </span>
-    </footer>`;
-    $tweet.append(content);
-    return $tweet;
+$(document).ready(() => {
+
+  // tweet element html content
+  const createTweetElement = (tweet) => {
+    return `<article>
+              <header>
+                <img src=${tweet.user.avatars} alt="my avatar">
+                <h3>${tweet.user.name}</h3>
+                <address>${tweet.user.handle}</address>
+              </header>
+              <p>${tweet.content.text}</p>
+              <footer>
+                <time>${tweetTimestamp(tweet)}</time>
+                <span class="fa-solid fa-flag" style="color:#4056A1">
+                  <i class="fa-solid fa-retweet" style="color:#4056A1"></i>
+                  <i class="fa-solid fa-heart" style="color:#4056A1"></i>
+                </span>
+              </footer>
+            </article>`;
   };
 
+  // calculate how many days ago tweet was written
   const tweetTimestamp = (tweet) => {
     const current = Date.now();
     const tweetTime = tweet.created_at;
@@ -48,11 +62,15 @@ $(document).ready(function() {
     return `${daysAgo} days ago`;
   };
 
-  const $tweet = createTweetElement(tweetData);
+  // fn to append each individual tweet from array of tweet data to the tweet-container
+  const renderTweets = function(tweets) {
+    // dynamically empty tweet-container of contents
+    $('#tweet-container').empty();
+    tweets.forEach((tweet) => {
+      $('#tweet-container').append(createTweetElement(tweet));
+    });
+  };
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-
-  $('#tweet-container').append($tweet);
+  renderTweets(tweetData);
 
 });
