beb75c2146b93ff4a8ecb11545b11a680b06436c
Brittany Cowper <brittferren@gmail.com>
2023-04-24 12:55:50 -0500

    implement timeago to date tweets, complete scroll-up btn feature

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,13 +5,11 @@
 $(document).ready(() => {
 
   //safely render possible insecure text by escaping it
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
-
-  // $("time.timeago").timeago(); //this is not a function?
 
   // tweet element html content
   const createTweetElement = (tweet) => {
@@ -23,7 +21,7 @@ $(document).ready(() => {
               </header>
               <p>${escape(tweet.content.text)}</p>
               <footer>
-                <time class="timeago">${tweet.created_at}</time>
+                <time class="timeago">${timeago().format(tweet.created_at)}</time>
                 <span class="fa-solid fa-flag" style="color:#4056A1">
                   <i class="fa-solid fa-retweet" style="color:#4056A1"></i>
                   <i class="fa-solid fa-heart" style="color:#4056A1"></i>
@@ -75,31 +73,31 @@ $(document).ready(() => {
 
     //use textarea input to verify conditionals
     if (!input.length) {
-      error = 'Cannot submit an empty tweet!';
+      let error = 'Cannot submit an empty tweet!';
       errorMsg.show();
       $("#error-msg").text(error);
       errorMsg.slideDown(600);
     } else if (input.length > 140) {
-      error = 'Your tweet is too long!';
+      let error = 'Your tweet is too long!';
       errorMsg.show();
       $("#error-msg").text(error);
       errorMsg.slideDown(600);
     } else {
       errorMsg.slideUp(600);
       $.ajax({
-          url: $(this).attr('action'),
-          type: "POST",
-          data: $(this).serialize(),
-          success: function () {
+        url: $(this).attr('action'),
+        type: "POST",
+        data: $(this).serialize(),
+        success: function () {
           //empty textarea
-            $('#tweet-text').val('');
+          $('#tweet-text').val('');
           //reset counter to 140
-            $('.counter').text('140');
-            loadTweets();
-          },
-          error: function (jqXHR, textStatus, errorThrown) {
-            console.log('error: ', error);
-          },
+          $('.counter').text('140');
+          loadTweets();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.log('error: ', errorThrown);
+        },
       });
     };
   });
@@ -107,6 +105,6 @@ $(document).ready(() => {
 
 
 $('.new-tweet p').append('<b>Error:</b> All tweets must contain at least one character. Your tweet currently does not.');
-      setTimeout(() => {
-        $('.new-tweet p').slideDown();
-      }, 600);
+setTimeout(() => {
+  $('.new-tweet p').slideDown();
+}, 600);
--- public/scripts/composer-scroll-up.js
+++ public/scripts/composer-scroll-up.js
@@ -4,19 +4,25 @@
 $(document).ready(function() {
   // scroll-to-top button appears on window scroll
   const toTopBtn = $('.scroll-up');
+  const navBtn = $('#write-tweet span');
   $(window).on('scroll', function() {
     if ($(window).scrollTop() > 20) {
+      //display scroll-up btn, hide nav bar btn
       toTopBtn.css('display', 'block');
+      navBtn.css('display', 'none');
     } else {
+      //display nav bar btn, hide scroll-up btn
       toTopBtn.css('display', 'none');
+      navBtn.css('display', 'block');
     }
   });
-  // remove button and focus on textarea when button is clicked
+  // return to top of page and focus on textarea when btn is clicked
   toTopBtn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: 0
     }, '300');
+    $('.new-tweet').css('display', 'block');
     $('.new-tweet textarea').focus();
   });
 });
